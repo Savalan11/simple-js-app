fae0185d46b316aaf75c018fb350fbd9404968d9
Savalan Suleymanli <savalan.suleymanli@gmail.com>
2022-03-07 15:37:49 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
 alert('Hello world');
 
-let favoriteFood = 'Ananas';
-document.write(favoriteFood);
+
+  let favoriteFood = [{
+
+  name: 'Ananas',
+  height: 0.8,
+  type: ['grass', 'poison']
+},
+
+{
+  name: 'Banana',
+  height: 2.1,
+  weight: 18,
+  type: ['fire']
+},
+
+{
+  name: 'Apple',
+  height: 1.8,
+  weight: 3.3,
+  type: ['dragon']
+},
+                      {
+  name: 'Pineapple',
+  height: 7,
+  weight: 2.5,
+  type: ['dragon']
+}];
+for (let i=0; i < favoriteFood.length; i++){
+  if(favoriteFood[i].height > 3){
+    document.write(favoriteFood[i].name + "( height : " + favoriteFood[i].height + ")" + "What a delicios food!!! <br>")
+  }else {
+  document.write(favoriteFood[i].name + "( height : " + favoriteFood[i].height + ")<br>")
+}
+}
