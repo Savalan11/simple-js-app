60337d22197eb0c7e917cd3176fdb1455198ed23
Savalan Suleymanli <savalan.suleymanli@gmail.com>
2022-03-09 22:23:07 +0100

    correction to pokemonlist
    correction to  some elements of pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
   let pokemonList = [{
 
   name: 'Bulbasaur',
-  height: 0.8,
-  weight: 2.1
+  height: 0.7,
+  weight: 6.9,
   type: ['grass', 'poison']
 },
 
 {
   name: 'Blastoise',
-  height: 2.1,
-  weight: 18,
+  height: 1.6,
+  weight: 85.5,
   type: ['fire']
 },
 
 {
   name: 'Caterpie',
-  height: 1.8,
-  weight: 3.3,
+  height: 0.3,
+  weight: 2.9,
   type: ['dragon']
 },
-                      {
+{
   name: 'Charmeleon',
-  height: 7,
-  weight: 2.5,
+  height: 1.1,
+  weight: 19,
   type: ['dragon']
 }];
 for (let i=0; i < pokemonList.length; i++){
   if(pokemonList[i].height > 3){
-    document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + ")" + "What a delicios figure!!! <br>")
+    document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + ")" + "What a unique figure!!! <br>")
   }else {
   document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + ")<br>")
 }
