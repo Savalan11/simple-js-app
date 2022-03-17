5cab44c4dc30e03438f35e7df87bb8910fa80468
Savalan Suleymanli <savalan.suleymanli@gmail.com>
2022-03-17 17:11:10 +0100

    forEach and IIFE
    With these amendments "forEach" and "IIFE"  functions have been added to the javascript file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,97 @@
-let pokemonList = [{
+// IIFE
+let pokemonRepository = (function() {
+  let pokemonList = [{
+      name: 'Bulbasaur',
+      height: 0.7,
+      weight: 6.9,
+      type: ['grass', 'poison']
+    },
 
-name: 'Bulbasaur',
-height: 0.7,
-weight: 6.9,
-type: ['grass', 'poison']
-},
+    {
+      name: 'Blastoise',
+      height: 3.6,
+      weight: 85.5,
+      type: ['fire']
+    },
 
-{
-name: 'Blastoise',
-height: 3.6,
-weight: 85.5,
-type: ['fire']
-},
+    {
+      name: 'Venusaur',
+      height: 1.3,
+      weight: 1.9,
+      type: ['dragon']
+    },
+    {
+      name: 'Caterpie',
+      height: 0.3,
+      weight: 2.9,
+      type: ['dragon']
+    },
 
-{
-name: 'Caterpie',
-height: 0.3,
-weight: 2.9,
-type: ['dragon']
-},
-{
-name: 'Charmeleon',
-height: 1.1,
-weight: 19,
-type: ['dragon']
-}];
-for (let i=0; i < pokemonList.length; i++){
-if(pokemonList[i].height > 3){
-  document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + ")" + "Wow that's big!!!<br>")
-}else {
-document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + ")<br>")
-}
-}
+    {
+      name: 'Charmander',
+      height: 2.4,
+      weight: 0.9,
+      type: ['dragon']
+    },
+
+    {
+      name: 'Butterfree',
+      height: 1.4,
+      weight: 0.9,
+      type: ['dragon']
+    },
+
+    {
+      name: 'Charmeleon',
+      height: 1.1,
+      weight: 19,
+      type: ['dragon']
+    },
+
+    {
+      name: 'Rayquaza',
+      height: 7,
+      weight: 206.5,
+      type: ['dragon']
+    }
+  ]
+  // Public oriented functions
+  function add(pokemon) {
+    pokemonList.push(pokemon)
+  }
+
+  function getAll() {
+    return pokemonList
+  }
+  // When cllicked shows pokemon name
+  function showDetails(pokemon) {
+    console.log(pokemon.name)
+  }
+
+  function addListener(button, pokemon) {
+    button.addEventListener("click", function() {
+      showDetails(pokemon)
+    })
+  }
+  // this function adds pokemon to ul as buttons
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listPokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    //addListener(button, pokemon);
+  }
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
+  }
+})();
+// Updated forEach loop
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon)
+});
